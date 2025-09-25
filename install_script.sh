daeaef9ce90e33640d9ee517e69aae585cb7398e
Hashaam Zafar <hashaam1217@gmail.com>
2025-09-25 14:51:00 -0700

    Added scripts

---
+++ install_script.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.config/nvim
+git clone https://github.com/hashaam1217/NvimConfig ~/.config/nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
